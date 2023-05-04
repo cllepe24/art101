72acf17e02712c28d6945fb630856c3cd837ae08
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-03 18:06:00 -0700

    the javascript didn't run again

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -20,16 +20,6 @@ function sortUserName() {
     return nameSorted;
 }
 
-function main() {
-    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
-    var sortedName = sortUserName(userName);
-    document.writeln("Here's your sorted name: " + sortedName + "</br></br>");
-    var randomName = randomizeName(userName);
-    document.writeln("And oh hey, I fixed your name.<br>Your new one is above.");
-    document.writeln("<div class='name'>" + randomName + "</div>")
-}
-
 //output
 document.writeln("Oh hey, I've fixed your name: ",
     sortUserName(), "</br>");
