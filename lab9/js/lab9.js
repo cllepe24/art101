b0fbab82c31e1012cbb9c9368a87b5fe84e5c793
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-10 20:44:55 -0700

    changed the msg2

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -21,7 +21,7 @@ window.onload = function () {
     // Step two
     new2El.innerHTML = "NO, CLICK ME!";
     new2El.addEventListener("click", msg2);
-    
+
 
     // Step three
     outputEl.appendChild(new1El);
@@ -43,5 +43,5 @@ function msg() {
 }
 
 function msg2() {
-    alert(">:(");
+    alert("ur mom >:)");
 }
