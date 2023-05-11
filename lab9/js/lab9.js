336139202bfc93daf31872e4b21ed961c7f504b1
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-10 18:59:05 -0700

    trying to add a button with javascript

---
+++ lab9/js/lab9.js
@@ -0,0 +1,16 @@
+//
+//Author: Corissa Lepe <cllepe@ucsc.edu>
+//Created: 10 May
+//
+
+window.onload = function (){
+    //step one
+    var outputEl = document.getElementById("output");
+    console.log("output: ", outputEl);
+
+    //step two
+    var newEl = document.createElement("button");
+
+    new1El.innerHTML = "Click Me!"
+
+}
