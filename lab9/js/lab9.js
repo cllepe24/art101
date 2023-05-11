9a6d2300f4839baaf683c534f836a1da10f8080d
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-10 19:56:28 -0700

    making edits to the javascript

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,25 +4,25 @@
 //
 
 window.onload = function () {
-    // Use getElementById() to find your output <div> and assign it to a variable outputEl (step 1)
+    // Step one
     var outputEl = document.getElementById("output");
     console.log("outputEl: ", outputEl);
     
-    // Create a new element with document.createElement("p") and assign it to a variable new1El (step 2)
+    // Step two
     var new1El = document.createElement("button");
 
-    // Change the html attribute of new1El to say "something new.""
+    // Step two
     new1El.innerHTML = "CLICK ME!";
     new1El.id = "new-one";
 
-    // Create a new element with document.createElement("p") and assign it to a variable new1El
+    // Step two
     var new2El = document.createElement("button");
     new1El.id = "new-two";
 
-    // Change the html attribute of new1El to say "something else.""
+    // Step two
     new2El.innerHTML = "NO, CLICK ME!";
 
-    // Append both new elements one at a time using appendChild() (step 3)
+    // Step three
     outputEl.appendChild(new1El);
     outputEl.appendChild(new2El);
 
@@ -30,6 +30,9 @@ window.onload = function () {
     // we do some stuff
     // new1El.style.color = "green";
     // outputEl.style.border = "dashed 3px purple";
+    // new1El.style.fontSize = "15px"
+    // new2El.style.fontSize = "15px"
+
     outputEl.className = "changed";
 
 }
