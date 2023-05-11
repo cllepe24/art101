d66c7aa48d949030d44ebec9ae22a73a00755851
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-10 19:09:16 -0700

    adding new elements and changing css in javascript

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -9,8 +9,23 @@ window.onload = function (){
     console.log("output: ", outputEl);
 
     //step two
-    var newEl = document.createElement("button");
+    var newEl = document.createElement("p");
 
-    new1El.innerHTML = "Click Me!"
+    new1El.innerHTML = "Something new.";
+    new1El.id = "new-one";
+
+    var new2El = document.createElement("p");
+    new1El.id = "new-two";
+
+    new2El.innerHTML = "Something else.";
+
+    //step three
+    outputEl.appendChild(new1El);
+    outputEl.appendChild(new2El);
+
+    //change some of the css
+    //new1El.style.color = "green";
+    //outputEl.style.border = "dashed 3px purple"
+    outputEl.className = "changed";
 
 }
