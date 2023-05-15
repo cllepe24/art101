e1df9f466cd6b352e5c9ec321ae08109f2c338e8
cllepe24 <130006985+cllepe24@users.noreply.github.com>
2023-05-15 14:58:03 -0700

    changed the js by getting rid of a lot of unnecessary parts and making it shorter, going off of wes's source code

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -5,57 +5,26 @@
 
 //functions
 
-//shuffleArray - take an array and shuffle the contents
-function shuffleArray (array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    //shuffle elements
-   while (0 !== currentIndex) {
-        //pick remaining element
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        //swap with current element 
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-
-//sortUserName to take user input and sort the letters of name
-function reorderUserName(word) {
-    var wordArray = word.toLowercase().split('');
-    var newArray = shuffleArray(wordArray);
-    return newArray.join('');
-
-}
-
-// given a string, return string in Title Case
-function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
-}
-
-//find the button element 
-buttonEl = document.getElementById("my-button");
-console.log("button element:", buttonEl);
-//find the form element 
-inputEl = document.getElementById("user-name");
-console.log("input element:", inputEl);
-//find output element
-outputEl = document.getElementById("output");
-console.log("output element:", outputEl);
-
-//add event listener to button
-buttonEl.addEventListener("click", function(){
-    //get the value from name element
-    var userName = inputEl.value;
-    //sort or shuffle value
-    var newName = toTitleCase(reorderUserName(userName));
-    //put value in output element
-    outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
-});
+function sortUserName(userName) {
+    // split the array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    //  userName.toLower().split("").sort().join("")
+    return nameSorted;
+  }
+  
+  // add button
+  var button = document.getElementById('my-button');
+  button.addEventListener('click', function() {
+    console.log("We got here.");
+    inputValue = document.getElementById('user-name').value;
+    var sortedName = sortUserName(inputValue);
+    console.log("Results", sortedName);
+    document.getElementById("output").innerHTML = sortedName;
+  });
